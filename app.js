4c3a7093c42329ae1d1fc4f7c16ab3431246fa8c
keshav kandoi <kandoikeshav2004@gmail.com>
2026-01-28 19:06:25 +0530

    styling the website by adding footer,navbar,image

--- app.js
+++ app.js
@@ -5,13 +5,18 @@ const Listing=require("./models/listing.js")
 const path=require("path")  //ejs k liye 
 const port=4000
 const methodOverride=require("method-override")
+const ejsMate=require("ejs-mate");
 
 const MONGO_URL="mongodb://127.0.0.1:27017/wonderlust"
 
+
+app.engine("ejs",ejsMate);
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"))
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname,"/public")))// ye static file like style.css likhe ke liye hai 
+
 
 
 main().then(()=>{
