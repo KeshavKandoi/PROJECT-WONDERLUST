16f6c75b691349204ffe1b13563ecfbaa808c2f2
keshav kandoi <kandoikeshav2004@gmail.com>
2026-02-01 23:30:40 +0530

    building review page

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const path=require("path")  //ejs k liye
 const port=4000
 const methodOverride=require("method-override")
 const ejsMate=require("ejs-mate");
-
+const wrapAsyc=require("./utils/wrapAsyc.js");
+const ExpressError=require("./utils/ExpressError.js");
+const { listingSchema,reviewSchema} =require("./schema.js")
+const Review =require("./models/review.js")
 const MONGO_URL="mongodb://127.0.0.1:27017/wonderlust"
 
 
@@ -35,24 +38,75 @@ app.get("/",(req,res)=>{
 })
 
 
+
+
+
+const validateListing=(req,res,next)=>{
+  let  {error}=listingSchema.validate(req.body);
+  
+  if(error){
+    let errMsg=error.details.map((el)=>el.message).join(",")
+     
+    throw new ExpressError(400,errMsg)
+  }else{
+    next();
+  }
+}
+
+
+
+
+
+const validateReview=(req,res,next)=>{
+  let  {error}=reviewSchema.validate(req.body);
+  
+  if(error){
+    let errMsg=error.details.map((el)=>el.message).join(",")
+     
+    throw new ExpressError(400,errMsg)
+  }else{
+    next();
+  }
+}
+
+
+
+
+
+
 app.get("/listings",async(req,res)=>{
   const allListings=await Listing.find({});
   res.render("listings/index",{allListings});
 })
-
+ 
 // new
 
 app.get("/listings/new",(req,res)=>{
   res.render("listings/new")
 })
 
-app.post("/listings",async(req,res)=>{
-   let{title,description,image,price,country,location}=req.body
+
+// show route
+
+app.get("/listings/:id",wrapAsyc(async(req,res)=>{
+  let{id}=req.params;
+  const listing= await Listing.findById(id).populate("reviews");
+  res.render("listings/show",{listing})
+}));
+
+
+
+
+// create route
+
+app.post("/listings",validateListing,wrapAsyc(async(req,res)=>{
+  
+    let{title,description,image,price,country,location}=req.body
 
   let sampleListing=new Listing({
         title:title,
         description:description,
-           // let listings=req.body.listings se sb nikal jaye gya ye sb nahi krna ho ga phir pass kr do newListing(req.body.listings;)
+                                                                  // let listings=req.body.listings se sb nikal jaye gya ye sb nahi krna ho ga phir pass kr do newListing(req.body.listings;)
         image: {
           filename: "listingimage",
           url: image,
@@ -63,7 +117,9 @@ app.post("/listings",async(req,res)=>{
       });
       await sampleListing.save()
       res.redirect("/listings")
+   
     })
+  );
 
 
 
@@ -77,10 +133,11 @@ app.post("/listings",async(req,res)=>{
 
 // update 
 
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", validateListing,wrapAsyc(async (req, res) => {
   let { id } = req.params;
   let { title, description, image, price, country, location } = req.body;
 
+  
   await Listing.findByIdAndUpdate(id, {
     title,
     description,
@@ -94,41 +151,67 @@ app.put("/listings/:id", async (req, res) => {
   });
 
   res.redirect("/listings");
-});
+})
 
-
+);
 
 // delete
 
-app.delete("/listings/:id",async(req,res)=>{
+app.delete("/listings/:id",wrapAsyc(async(req,res)=>{
   let { id } = req.params;
   await Listing.findByIdAndDelete(id);
   res.redirect("/listings");
 
 })
 
+);
+// Reviews
+// post route
+
+app.post("/listings/:id/reviews",validateReview,wrapAsyc(async(req,res)=>{
+  let listing=await Listing.findById(req.params.id)
+  let newReview=new Review(req.body.review);
+
+  listing.reviews.push(newReview);
+
+  await newReview.save();
+  await listing.save();
+  
+res.redirect(`/listings/${listing ._id}`);
+
+})) ;
+
+
+// delete review 
+
+app.delete("/listings/:id/reviews/:reviewId",wrapAsyc(async(req,res)=>{
+  let{id,reviewId}=req.params;
+
+  await Listing.findByIdAndUpdate(id,{$pull:{review:reviewId}});
+  
+  await Review.findByIdAndDelete(reviewId);
+
+  res.redirect(`/listings/${id}`)
+}))
 
 
 
 
-app.get("/listings/:id",async(req,res)=>{
-  let{id}=req.params;
-  const listing= await Listing.findById(id);
-  res.render("listings/show.ejs",{listing})
+
+
+
+
+
+
+
+
+
+
+
+app.use((req,res,next)=>{
+  next(new ExpressError(404,"Page Not Found")); //baki sabhi route like/hi,/random sabke liye page not found aagye ga
+
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -146,6 +229,14 @@ app.get("/listings/:id",async(req,res)=>{
 //   console.log("sample was saved");
 //   res.send("successful")
 // })
+
+
+app.use((err,req,res,next)=>{  
+  let{statusCode=500,message="Something went wrong"}=err;
+  res.status(statusCode).render("listings/error",{err})
+// res.status(statusCode).send(message)
+})
+
 
 app.listen(port,()=>{
   console.log("Server is listening to port no 4000")
--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose=require("mongoose")
 const Schema=mongoose.Schema
+const Review=require("./review.js")
 
 const listingSchema=new Schema({
   title:{
@@ -8,15 +9,7 @@ const listingSchema=new Schema({
   },
   description:String,
  
-  // image: {
-  //   filename: String,
-  //   url: {
-  //     type: String,
-  //     default:
-  //       "https://unsplash.com/photos/two-figures-walk-towards-a-large-cathedral-in-fog-JFet8xoge7U",
-  //       set:(v)=> v===""?"https://unsplash.com/photos/two-figures-walk-towards-a-large-cathedral-in-fog-JFet8xoge7U":v,
-  //   },
-  // },
+
   image: {
     filename: String,
     url: {
@@ -34,6 +27,19 @@ const listingSchema=new Schema({
   price:Number,
   location:String,
   country:String,
+
+  reviews:[
+    {
+      type:Schema.Types.ObjectId,
+      ref:"Review"
+    },
+  ],
+});
+
+listingSchema.post("findOneAndDelete",async(listing)=>{
+  if(listing){
+    await Review.deleteMany({_id:{$in:listing.reviews}})
+  }
 });
 
 const Listing=mongoose.model("Listing",listingSchema);
