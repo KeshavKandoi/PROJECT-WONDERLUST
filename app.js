c51b9e46691e5a8de2fec4ad3c8e9cedb91418b9
keshav kandoi <kandoikeshav2004@gmail.com>
2026-02-03 18:55:31 +0530

    adding signup,loginpage,review

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 const express=require("express")
 const app=express();
 const mongoose=require("mongoose")
-const Listing=require("./models/listing.js")
+// const wrapAsyc=require("./utils/wrapAsyc.js");
+// const Listing=require("./models/listing.js")
+// const { listingSchema,reviewSchema} =require("./schema.js") -- ye sab abhi use nahi ho rha hai 
+// const Review =require("./models/review.js")
+
 const path=require("path")  //ejs k liye 
 const port=4000
 const methodOverride=require("method-override")
 const ejsMate=require("ejs-mate");
-const wrapAsyc=require("./utils/wrapAsyc.js");
+
 const ExpressError=require("./utils/ExpressError.js");
-const { listingSchema,reviewSchema} =require("./schema.js")
-const Review =require("./models/review.js")
+
 const MONGO_URL="mongodb://127.0.0.1:27017/wonderlust"
+
+const listingRouter=require("./routes/listing.js");
+const reviewRouter=require("./routes/review.js")
+const userRouter=require("./routes/user.js")
+
+
+const session=require("express-session")//session use karne ke liye 
+
+const flash=require("connect-flash")
+
+const passport=require("passport")
+const LocalStrategy=require("passport-local");
+const User=require("./models/user.js");
 
 
 app.engine("ejs",ejsMate);
@@ -19,6 +35,64 @@ app.set("views",path.join(__dirname,"views"))
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname,"/public")))// ye static file like style.css likhe ke liye hai 
+
+
+// session
+
+const sessionOptions={
+  secret:"mysupersecretcode",
+  resave:false,
+  saveUninitialized:true,
+  cookie:{
+    expires:Date.now()+7*24*60*60*1000,  //7 days 24 hours 60 minutes 60 seconds 1000 milliseconds ,millisecond mai pass karna hota hai 
+    maxAge:7*24*60*60*1000,   
+    httpOnly:true,
+
+  },
+};
+
+app.use(session(sessionOptions));
+app.use(flash())
+
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+ 
+
+
+
+app.use((req,res,next)=>{
+  res.locals.success=req.flash("success");
+  res.locals.error=req.flash("error")
+  next()
+})
+ 
+
+app.use("/listings" , listingRouter);
+app.use("/listings/:id/reviews",reviewRouter) //iske "/listings/:id/reviews" badle review use kro review.js mai 
+app.use("/",userRouter)
+
+
+// app.get("/demouser",async(req,res)=>{
+//   let fakeUser=new User({
+//     email:"student@gmail.com",
+//     username:"delt-student"
+//   });
+
+//   let registeredUser=await User.register(fakeUser,"helloworld")
+//   res.send(registeredUser);
+// })
+
+
+
+ 
+
 
 
 
@@ -41,194 +115,12 @@ app.get("/",(req,res)=>{
 
 
 
-const validateListing=(req,res,next)=>{
-  let  {error}=listingSchema.validate(req.body);
-  
-  if(error){
-    let errMsg=error.details.map((el)=>el.message).join(",")
-     
-    throw new ExpressError(400,errMsg)
-  }else{
-    next();
-  }
-}
-
-
-
-
-
-const validateReview=(req,res,next)=>{
-  let  {error}=reviewSchema.validate(req.body);
-  
-  if(error){
-    let errMsg=error.details.map((el)=>el.message).join(",")
-     
-    throw new ExpressError(400,errMsg)
-  }else{
-    next();
-  }
-}
-
-
-
-
-
-
-app.get("/listings",async(req,res)=>{
-  const allListings=await Listing.find({});
-  res.render("listings/index",{allListings});
-})
- 
-// new
-
-app.get("/listings/new",(req,res)=>{
-  res.render("listings/new")
-})
-
-
-// show route
-
-app.get("/listings/:id",wrapAsyc(async(req,res)=>{
-  let{id}=req.params;
-  const listing= await Listing.findById(id).populate("reviews");
-  res.render("listings/show",{listing})
-}));
-
-
-
-
-// create route
-
-app.post("/listings",validateListing,wrapAsyc(async(req,res)=>{
-  
-    let{title,description,image,price,country,location}=req.body
-
-  let sampleListing=new Listing({
-        title:title,
-        description:description,
-                                                                  // let listings=req.body.listings se sb nikal jaye gya ye sb nahi krna ho ga phir pass kr do newListing(req.body.listings;)
-        image: {
-          filename: "listingimage",
-          url: image,
-        },
-        price:price,
-        country:country,
-        location:location,
-      });
-      await sampleListing.save()
-      res.redirect("/listings")
-   
-    })
-  );
-
-
-
-
-// edit
-  app.get("/listings/:id/edit",async(req,res)=>{
-    let{id}=req.params;
-    const listings=await Listing.findById(id);
-    res.render("listings/edit",{listings})
-  })
-
-// update 
-
-app.put("/listings/:id", validateListing,wrapAsyc(async (req, res) => {
-  let { id } = req.params;
-  let { title, description, image, price, country, location } = req.body;
-
-  
-  await Listing.findByIdAndUpdate(id, {
-    title,
-    description,
-    image: {
-      filename: "listingimage",
-      url: image,
-    },
-    price,
-    country,
-    location,
-  });
-
-  res.redirect("/listings");
-})
-
-);
-
-// delete
-
-app.delete("/listings/:id",wrapAsyc(async(req,res)=>{
-  let { id } = req.params;
-  await Listing.findByIdAndDelete(id);
-  res.redirect("/listings");
-
-})
-
-);
-// Reviews
-// post route
-
-app.post("/listings/:id/reviews",validateReview,wrapAsyc(async(req,res)=>{
-  let listing=await Listing.findById(req.params.id)
-  let newReview=new Review(req.body.review);
-
-  listing.reviews.push(newReview);
-
-  await newReview.save();
-  await listing.save();
-  
-res.redirect(`/listings/${listing ._id}`);
-
-})) ;
-
-
-// delete review 
-
-app.delete("/listings/:id/reviews/:reviewId",wrapAsyc(async(req,res)=>{
-  let{id,reviewId}=req.params;
-
-  await Listing.findByIdAndUpdate(id,{$pull:{review:reviewId}});
-  
-  await Review.findByIdAndDelete(reviewId);
-
-  res.redirect(`/listings/${id}`)
-}))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.use((req,res,next)=>{
-  next(new ExpressError(404,"Page Not Found")); //baki sabhi route like/hi,/random sabke liye page not found aagye ga
+next(new ExpressError(404,"Page Not Found")); //baki sabhi route like/hi,/random sabke liye page not found aagye ga
 
 })
 
 
-
-
-
-// app.get("/testListing",async(req,res)=>{
-//   let sampleListing=new Listing({
-//     title:"My New Villa",
-//     description:"By the beach",
-//     price:1200,
-//     location:"calangute,Goa",
-//     country:"India",
-//   });
-//   await sampleListing.save()
-//   console.log("sample was saved");
-//   res.send("successful")
-// })
 
 
 app.use((err,req,res,next)=>{  
---
+++ routes/listing.js
@@ -0,0 +1,139 @@
+ const express=require("express");
+ const router=express.Router()
+ const wrapAsyc=require("../utils/wrapAsyc.js");
+ const { listingSchema,reviewSchema} =require("../schema.js")
+ const ExpressError=require("../utils/ExpressError.js");
+ const Listing=require("../models/listing.js")
+
+
+
+
+
+
+
+ const validateListing=(req,res,next)=>{
+  let  {error}=listingSchema.validate(req.body);
+  
+  if(error){
+    let errMsg=error.details.map((el)=>el.message).join(",")
+     
+    throw new ExpressError(400,errMsg)
+  }else{
+    next();
+  }
+}
+
+
+
+// INDEX ROUTE
+
+// router.get("/listings",async(req,res)=>{
+//   const allListings=await Listing.find({});
+//   res.render("listings/index",{allListings});
+// })
+ 
+
+router.get("/",async(req,res)=>{
+  const allListings=await Listing.find({});
+  res.render("listings/index",{allListings});
+})
+ 
+// NEW ROUTE
+
+router.get("/new",(req,res)=>{
+  res.render("listings/new")
+})
+
+
+// show route
+
+router.get("/:id",wrapAsyc(async(req,res)=>{
+  let{id}=req.params;
+  const listing= await Listing.findById(id).populate("reviews");
+  if(!listing){
+    req.flash("error","Listing you requested for does not exist")
+   return res.redirect("/listings")
+  }
+  res.render("listings/show",{listing})
+}));
+
+
+
+
+// create route
+
+router.post("/",validateListing,wrapAsyc(async(req,res)=>{
+  
+    let{title,description,image,price,country,location}=req.body
+
+  let sampleListing=new Listing({
+        title:title,
+        description:description,
+                                                                  // let listings=req.body.listings se sb nikal jaye gya ye sb nahi krna ho ga phir pass kr do newListing(req.body.listings;)
+        image: {
+          filename: "listingimage",
+          url: image,
+        },
+        price:price,
+        country:country,
+        location:location,
+      });
+      await sampleListing.save()
+      req.flash("success","New Listing Created!")
+      res.redirect("/listings")
+   
+    })
+  );
+
+
+
+
+// edit
+  router.get("/:id/edit",async(req,res)=>{
+    let{id}=req.params;
+    const listings=await Listing.findById(id);
+    if(!listings){
+      req.flash("error","Listing you requested for does not exist")
+     return res.redirect("/listings")
+    }
+    res.render("listings/edit",{listings})
+  })
+
+// update 
+
+router.put("/:id", validateListing,wrapAsyc(async (req, res) => {
+  let { id } = req.params;
+  let { title, description, image, price, country, location } = req.body;
+
+  
+  await Listing.findByIdAndUpdate(id, {
+    title,
+    description,
+    image: {
+      filename: "listingimage",
+      url: image,
+    },
+    price,
+    country,
+    location,
+  });
+  req.flash("success","Listing updated")
+
+  res.redirect("/listings");
+})
+
+);
+
+// DELETE
+
+router.delete("/:id",wrapAsyc(async(req,res)=>{
+  let { id } = req.params;
+  await Listing.findByIdAndDelete(id);
+  req.flash("success","Listing Deleted!")
+  res.redirect("/listings");
+
+})
+
+);
+
+module.exports=router;
